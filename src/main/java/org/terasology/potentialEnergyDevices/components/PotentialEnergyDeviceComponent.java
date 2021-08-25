3795bd716785d2b7128a3dbf141f54cc9f0a6e5f
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 18:59:54 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#5)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/potentialEnergyDevices/components/PotentialEnergyDeviceComponent.java
+++ src/main/java/org/terasology/potentialEnergyDevices/components/PotentialEnergyDeviceComponent.java
@@ -1,27 +1,14 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.potentialEnergyDevices.components;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
 import org.terasology.engine.world.block.ForceBlockActive;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.module.inventory.components.ItemDifferentiating;
 
 @ForceBlockActive
-public class PotentialEnergyDeviceComponent implements Component, ItemDifferentiating {
+public class PotentialEnergyDeviceComponent implements Component<PotentialEnergyDeviceComponent>, ItemDifferentiating {
     @Replicate
     public float currentStoredEnergy;
     @Replicate
@@ -57,5 +44,11 @@ public class PotentialEnergyDeviceComponent implements Component, ItemDifferenti
         int result = (currentStoredEnergy != +0.0f ? Float.floatToIntBits(currentStoredEnergy) : 0);
         result = 31 * result + (maximumStoredEnergy != +0.0f ? Float.floatToIntBits(maximumStoredEnergy) : 0);
         return result;
+    }
+
+    @Override
+    public void copyFrom(PotentialEnergyDeviceComponent other) {
+        this.currentStoredEnergy = other.currentStoredEnergy;
+        this.maximumStoredEnergy = other.maximumStoredEnergy;
     }
 }
