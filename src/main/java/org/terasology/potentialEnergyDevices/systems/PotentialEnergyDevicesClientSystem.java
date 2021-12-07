9380a2d71e45a1a08b0b0d9ead6745ca20b94b0d
Nail Khanipov <foxflameless@gmail.com>
2021-12-07 23:21:46 +0300

    feat(gestaltv7-eventsystem): Migration Event and @ReceiveEvent to gestalt's (#6)

--- src/main/java/org/terasology/potentialEnergyDevices/systems/PotentialEnergyDevicesClientSystem.java
+++ src/main/java/org/terasology/potentialEnergyDevices/systems/PotentialEnergyDevicesClientSystem.java
@@ -3,13 +3,13 @@
 package org.terasology.potentialEnergyDevices.systems;
 
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
+import org.terasology.module.inventory.ui.GetItemTooltip;
 import org.terasology.nui.widgets.TooltipLine;
 import org.terasology.potentialEnergyDevices.components.PotentialEnergyDeviceComponent;
-import org.terasology.module.inventory.ui.GetItemTooltip;
 
 @RegisterSystem(RegisterMode.CLIENT)
 public class PotentialEnergyDevicesClientSystem extends BaseComponentSystem {
